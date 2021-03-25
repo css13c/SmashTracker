b47478c5f8804d96ccee54ca3580c984389a94ec
Connor Smith <csmith78810@gmail.com>
2021-03-24 22:33:37 -0500

    A little bit more work on Player Info

--- src/components/PlayerInfo.vue
+++ src/components/PlayerInfo.vue
@@ -1,4 +1,21 @@
 <template>
-  <div >
-  </div>
+  <div>{{player.name}}</div>
+  <div>{{player.rating.estimate()}}</div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    player: {
+      name: String,
+
+    }
+  },
+  setup(props) {
+    
+  },
+})
+</script>
+
